e2ad2add54d8ce38324aaa1fa1331cde7ea2ee97
Kostas Konovodoff <kostas.konovodoff@epitech.eu>
2017-11-27 11:32:05 +0100

    Update to pamela repo

--- container_func.h
+++
@@ -1,7 +0,0 @@
-#ifndef _CONTAINER_H_
-# define _CONTAINER_H_
-
-int	close_container(const char *user);
-int	create_container(const char *user, const char *password);
-void	change_passphrase(const char *old_pass, const char *new_pass, const char *user);
-#endif /* !_CONTAINER_H_ */
